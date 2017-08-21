74c84bc9115d88e5abd02549e9874ee74baa964b
sunny <wangsg@bankhui.com>
2017-08-21 17:51:36 +0800

    扩大button的点击范围

---
+++ UIButton/UIButton+test.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+test.h
+//  testfiripa
+//
+//  Created by 王帅广 on 2017/8/21.
+//  Copyright © 2017年 王帅广. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (test)
+//扩大button的点击范围  给button添加属性
+@property(nonatomic, assign) UIEdgeInsets hitTestEdgeInsets;
+
+@end
---
+++ UIButton/UIButton+test.m
@@ -0,0 +1,45 @@
+//
+//  UIButton+test.m
+//  testfiripa
+//
+//  Created by 王帅广 on 2017/8/21.
+//  Copyright © 2017年 王帅广. All rights reserved.
+//
+
+#import "UIButton+test.h"
+#import <objc/runtime.h>
+
+@implementation UIButton (test)
+@dynamic hitTestEdgeInsets;
+
+static const NSString *KEY_HIT_TEST_EDGE_INSETS = @"HitTestEdgeInsets";
+
+-(void)setHitTestEdgeInsets:(UIEdgeInsets)hitTestEdgeInsets {
+    NSValue *value = [NSValue value:&hitTestEdgeInsets withObjCType:@encode(UIEdgeInsets)];
+    objc_setAssociatedObject(self, &KEY_HIT_TEST_EDGE_INSETS, value, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(UIEdgeInsets)hitTestEdgeInsets {
+    NSValue *value = objc_getAssociatedObject(self, &KEY_HIT_TEST_EDGE_INSETS);
+    if(value) {
+        UIEdgeInsets edgeInsets;
+        [value getValue:&edgeInsets];
+        return edgeInsets;
+    }else {
+        return UIEdgeInsetsZero;
+    }
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    if(UIEdgeInsetsEqualToEdgeInsets(self.hitTestEdgeInsets, UIEdgeInsetsZero) || !self.enabled || self.hidden) {
+        return [super pointInside:point withEvent:event];
+    }
+    
+    CGRect relativeFrame = self.bounds;
+    CGRect hitFrame = UIEdgeInsetsInsetRect(relativeFrame, self.hitTestEdgeInsets);
+            //判断这个点是否在这个范围内
+    return CGRectContainsPoint(hitFrame, point);
+}
+
+
+@end
